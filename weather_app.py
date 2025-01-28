6d5a14fb1f8891ed3d1340d547ce8359e0196b50
Nitishkothakonda199920 <nitishkothakonda20@gmail.com>
2025-01-28 16:24:44 -0600

    Add weather_app.py

---
+++ weather_app.py
@@ -0,0 +1,29 @@
+def get_weather(city):
+    api_key = "f025766cf5e6ec985a3ddfdf9c257989"
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {"q": city, "appid": api_key, "units": "metric"}
+    
+    try:
+        response = requests.get(base_url, params=params)
+        response.raise_for_status()
+        data = response.json()
+         # Extract weather details
+        temp = data['main']['temp']
+        weather = data['weather'][0]['description']
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        
+        print(f"Weather in {city}:")
+        print(f"Temperature: {temp}°C")
+        print(f"Condition: {weather.capitalize()}")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} m/s")
+    except requests.exceptions.requestexception as e:
+        print('error fetching data:',e)
+    except keyerror:
+        print('city not found,please check the city name.')
+
+if __name__=='__main__':
+    city=input('enter city name:')
+    get_weather(city)
+        
